7bb7b9375107e7999da914e6eb541eff770c6e1d
m10maeda <mio.maeda.kari@gmail.com>
2021-05-24 22:57:26 +0900

    Add IPlayerRuleFactory interface

--- src/domain/models/playersRule/index.ts
+++ src/domain/models/playersRule/index.ts
@@ -1,3 +1,4 @@
+export * from './IPlayerRuleFactory';
 export { default as PlayersRule } from './PlayersRule';
 export { default as PlayersRuleId } from './PlayersRuleId';
 export { default as PlayersRuleType } from './PlayersRuleType';
