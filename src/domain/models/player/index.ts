d2c360e9f1c15dd0520cb42a87363de9ce973810
m10maeda <mio.maeda.kari@gmail.com>
2021-05-24 22:39:02 +0900

    Add PlayerCount domain object

--- src/domain/models/player/index.ts
+++ src/domain/models/player/index.ts
@@ -1,3 +1,4 @@
 export { default as Player } from './Player';
+export { default as PlayerCount } from './PlayerCount';
 export { default as PlayerId } from './PlayerId';
 export { default as PlayerName } from './PlayerName';
