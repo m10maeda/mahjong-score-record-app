4d637773a26f60cf24af810aace452976ee5e346
m10maeda <mio.maeda.kari@gmail.com>
2021-05-30 23:14:53 +0900

    Add EntryPlayerListMinSpecification domain object

--- src/domain/models/entryPlayerList/index.ts
+++ src/domain/models/entryPlayerList/index.ts
@@ -1,2 +1,3 @@
 export { default as EntryPlayerListId } from './EntryPlayerListId';
+export { default as EntryPlayerListMinSpecification } from './EntryPlayerListMinSpecification';
 export { default as EntryPlayers } from './EntryPlayers';
