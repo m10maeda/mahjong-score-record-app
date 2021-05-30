a53602ecfb722119546c5a7952b6c6a01ced21d1
m10maeda <mio.maeda.kari@gmail.com>
2021-05-30 22:51:58 +0900

    Add EntryPlayers domain object

--- src/domain/models/entryPlayerList/index.ts
+++ src/domain/models/entryPlayerList/index.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as EntryPlayerListId } from './EntryPlayerListId';
+export { default as EntryPlayers } from './EntryPlayers';
