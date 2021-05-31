095ad903a2a4aae9920d948e6f1101392fda0f98
m10maeda <mio.maeda.kari@gmail.com>
2021-05-31 22:48:38 +0900

    Add IEntryPlayerListFactory interface

--- src/domain/models/entryPlayerList/index.ts
+++ src/domain/models/entryPlayerList/index.ts
@@ -2,3 +2,4 @@ export { default as EntryPlayerList } from './EntryPlayerList';
 export { default as EntryPlayerListId } from './EntryPlayerListId';
 export { default as EntryPlayerListMinSpecification } from './EntryPlayerListMinSpecification';
 export { default as EntryPlayers } from './EntryPlayers';
+export * from './IEntryPlayerListFactory';
