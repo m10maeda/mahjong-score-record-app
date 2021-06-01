5ca2fb6a094d1ef7718ea5a039f540294bd9be05
m10maeda <mio.maeda.kari@gmail.com>
2021-06-01 22:24:51 +0900

    Add ChipScoreTableMinSpecification domain object

--- src/domain/models/chipScoreTable/index.ts
+++ src/domain/models/chipScoreTable/index.ts
@@ -1,3 +1,4 @@
 export { default as ChipScoreTableId } from './ChipScoreTableId';
+export { default as ChipScoreTableMinSpecification } from './ChipScoreTableMinSpecification';
 export { default as PlayerChipScore } from './PlayerChipScore';
 export { default as PlayerChipScores } from './PlayerChipScores';
