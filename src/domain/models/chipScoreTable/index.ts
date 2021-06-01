962f54ca8fac81630c011dfd6a2902b5e9783637
m10maeda <mio.maeda.kari@gmail.com>
2021-06-01 22:28:14 +0900

    Add IChipScoreTableFactory interface

--- src/domain/models/chipScoreTable/index.ts
+++ src/domain/models/chipScoreTable/index.ts
@@ -1,5 +1,6 @@
 export { default as ChipScoreTable } from './ChipScoreTable';
 export { default as ChipScoreTableId } from './ChipScoreTableId';
 export { default as ChipScoreTableMinSpecification } from './ChipScoreTableMinSpecification';
+export * from './IChipScoreTableFactory';
 export { default as PlayerChipScore } from './PlayerChipScore';
 export { default as PlayerChipScores } from './PlayerChipScores';
