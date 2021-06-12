21c733bd449a0a7a19cd3037962170a3650d148b
m10maeda <mio.maeda.kari@gmail.com>
2021-06-12 23:01:12 +0900

    Add GameId domain object

---
+++ src/domain/models/game/GameId.test.ts
@@ -0,0 +1,45 @@
+import GameId from './GameId';
+
+describe('不正な値で生成しようとするとエラーを投げる', () => {
+  test.each`
+    value | description
+    ${''} | ${'空文字（`""`）'}
+  `('$description', ({ value }: { value: string }) => {
+    expect(() => {
+      // eslint-disable-next-line no-new
+      new GameId(value);
+    }).toThrowError();
+  });
+});
+
+test('toString メソッドが期待した文字列を返す', () => {
+  const value = '1234567890';
+  const id = new GameId(value);
+
+  expect(id.toString()).toBe(value);
+});
+
+describe('equals メソッドが正しく比較する', () => {
+  test.each`
+    a      | b      | expected
+    ${'0'} | ${'0'} | ${true}
+    ${'0'} | ${'1'} | ${false}
+    ${'1'} | ${'0'} | ${false}
+  `(
+    '$a equals $b is $expected',
+    ({
+      a: _a,
+      b: _b,
+      expected,
+    }: {
+      a: string;
+      b: string;
+      expected: boolean;
+    }) => {
+      const a = new GameId(_a);
+      const b = new GameId(_b);
+
+      expect(a.equals(b)).toBe(expected);
+    },
+  );
+});
