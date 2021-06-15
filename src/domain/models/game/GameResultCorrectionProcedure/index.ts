ef0d076fcc895ddc915714a9e482574b08743c63
m10maeda <mio.maeda.kari@gmail.com>
2021-06-15 22:49:56 +0900

    Add GameResultAdjuster domain object

--- src/domain/models/game/GameResultCorrectionProcedure/index.ts
+++ src/domain/models/game/GameResultCorrectionProcedure/index.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
+export { default as GameResultAdjuster } from './GameResultAdjuster';
 export { default as GameResultComplementer } from './GameResultComplementer';
