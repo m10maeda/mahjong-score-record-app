4dff9077f0002ecfa985c0d2a9260e165edcca61
m10maeda <mio.maeda.kari@gmail.com>
2021-06-15 22:50:13 +0900

    Add GameResultCorrectionProcedure domain object

--- src/domain/models/game/GameResultCorrectionProcedure/index.ts
+++ src/domain/models/game/GameResultCorrectionProcedure/index.ts
@@ -1,2 +1,3 @@
 export { default as GameResultAdjuster } from './GameResultAdjuster';
 export { default as GameResultComplementer } from './GameResultComplementer';
+export { default } from './GameResultCorrectionProcedure';
