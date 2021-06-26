7c0e4e98ae481f2d84d4380a90311a60be6c8391
m10maeda <mio.maeda.kari@gmail.com>
2021-06-26 23:27:15 +0900

    Add RemovedPlayerEvent domain object

--- src/domain/models/entryPlayerList/EntryPlayerListEvent/index.ts
+++ src/domain/models/entryPlayerList/EntryPlayerListEvent/index.ts
@@ -1,2 +1,3 @@
 export { default as EntriedPlayerEvent } from './EntriedPlayerEvent';
 export { default as EntryPlayerListEvent } from './EntryPlayerListEvent';
+export { default as RemovedPlayerEvent } from './RemovedPlayerEvent';
--- src/domain/models/entryPlayerList/index.ts
+++ src/domain/models/entryPlayerList/index.ts
@@ -2,6 +2,7 @@ export { default as EntryPlayerList } from './EntryPlayerList';
 export {
   EntriedPlayerEvent,
   EntryPlayerListEvent,
+  RemovedPlayerEvent,
 } from './EntryPlayerListEvent';
 export { default as EntryPlayerListId } from './EntryPlayerListId';
 export { default as EntryPlayerListMinSpecification } from './EntryPlayerListMinSpecification';
