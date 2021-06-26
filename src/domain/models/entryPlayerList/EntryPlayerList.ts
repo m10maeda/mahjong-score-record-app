9751f68beeee30af08619fd2eca60beb160eff8a
m10maeda <mio.maeda.kari@gmail.com>
2021-06-27 00:12:28 +0900

    Make EntryPlayerList to publish EntryPlayerListEvent

--- src/domain/models/entryPlayerList/EntryPlayerList.ts
+++ src/domain/models/entryPlayerList/EntryPlayerList.ts
@@ -1,4 +1,10 @@
 import { Player, PlayerCount, PlayerId } from '../player';
+import {
+  ChangedPlayerEvent,
+  EntriedPlayerEvent,
+  EntryPlayerListEventPublisher,
+  RemovedPlayerEvent,
+} from './EntryPlayerListEvent';
 import EntryPlayerListId from './EntryPlayerListId';
 import EntryPlayerListMinSpecification from './EntryPlayerListMinSpecification';
 import EntryPlayers from './EntryPlayers';
@@ -7,6 +13,8 @@ export default class EntryPlayerList implements Iterable<PlayerId> {
   public readonly id: EntryPlayerListId;
 
   private players: EntryPlayers;
+
+  private readonly publisher: EntryPlayerListEventPublisher;
 
   public get count(): PlayerCount {
     return this.players.count;
@@ -36,6 +44,8 @@ export default class EntryPlayerList implements Iterable<PlayerId> {
 
   public add(player: Player): void {
     this.players = this.players.add(player.id);
+
+    this.publisher.publish(new EntriedPlayerEvent(this.id, player.id));
   }
 
   public remove(
@@ -50,19 +60,27 @@ export default class EntryPlayerList implements Iterable<PlayerId> {
     }
 
     this.players = newPlayers;
+
+    this.publisher.publish(new RemovedPlayerEvent(this.id, player.id));
   }
 
   public change(fromPlayer: Player, toPlayer: Player): void {
     this.players = this.players.change(fromPlayer.id, toPlayer.id);
+
+    this.publisher.publish(
+      new ChangedPlayerEvent(this.id, toPlayer.id, fromPlayer.id),
+    );
   }
 
   public constructor(
     id: EntryPlayerListId,
     players: EntryPlayers,
     minSpec: EntryPlayerListMinSpecification,
+    publisher: EntryPlayerListEventPublisher,
   ) {
     this.id = id;
     this.players = players;
+    this.publisher = publisher;
 
     if (!minSpec.isSatisfiedBy(players)) {
       throw new RangeError(
