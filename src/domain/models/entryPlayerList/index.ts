ea31775d325d154b0108ca7d620ff55e6d2c8a7f
m10maeda <mio.maeda.kari@gmail.com>
2021-06-26 23:22:27 +0900

    Add EntryPlayerListEvent domain object

--- src/domain/models/entryPlayerList/index.ts
+++ src/domain/models/entryPlayerList/index.ts
@@ -1,4 +1,5 @@
 export { default as EntryPlayerList } from './EntryPlayerList';
+export { EntryPlayerListEvent } from './EntryPlayerListEvent';
 export { default as EntryPlayerListId } from './EntryPlayerListId';
 export { default as EntryPlayerListMinSpecification } from './EntryPlayerListMinSpecification';
 export { default as EntryPlayers } from './EntryPlayers';
